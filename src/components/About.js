8a31f931d18b960f3ac476d12e5932863085c69f
Tamara Ackovska <tamaraatletisimo@yahoo.com>
2022-08-12 20:05:09 +0200

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
-import data from "../data";
+import React from "react";
 
 const About = () => {
-  const [cards, setCards] = useState(data);
-
   return (
-    <section style={{ textAlign: "center" ,position:"relative",top:'40px' }}>
-      <h3>About this project</h3>
-      <p>
+    <div id="about">
+      <h2 className="about font-semibold text-sm mb-8">About</h2>
+      <p className="paragraph">
         The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform
         that elevates your screen to a more comfortable viewing height. Placing
         your monitor at eye level has the potential to improve your posture and
         make you more comfortable while at work, helping you stay focused on the
         task at hand.
       </p>
-      <p>
+      <p className="paragraph">
         Featuring artisan craftsmanship, the simplicity of design creates extra
         desk space below your computer to allow notepads, pens, and USB sticks
         to be stored under the stand.
       </p>
-      <div>
-        {cards.map((card) => {
-          const { id, title, pledge, description, left, button } = card;
-          return (
-            <article key={id}>
-              <h3>{title}</h3>
-              <p>{pledge}</p>
-              <p>{description}</p>
-              <p>
-                {left} {""} <span>left</span>
-              </p>
-              <button class="btn" style={{ padding: "10px" , position:'relative',left:'280px',  borderRadius: "20px",
-  fontSize: "16px"}}>
-                {button}
-              </button>
-            </article>
-          );
-        })}
-      </div>
-    </section>
+    </div>
   );
 };
-
 export default About;
